417129210ca242b89f6b85aa3f13975d9f61a773
Fady Mohsen <82896079+fadymohsen@users.noreply.github.com>
2026-07-20 02:14:04 +0300

    add: node modules

---
+++ .next/server/chunks/ssr/_next-internal_server_app_[locale]_news_page_actions_0thjjab.js
@@ -0,0 +1,3 @@
+module.exports=[5433,(a,b,c)=>{}];
+
+//# sourceMappingURL=_next-internal_server_app_%5Blocale%5D_news_page_actions_0thjjab.js.map
---
+++ .next/server/chunks/ssr/_next-internal_server_app_[locale]_training_page_actions_1rr5ycl.js
@@ -0,0 +1,3 @@
+module.exports=[79084,(a,b,c)=>{}];
+
+//# sourceMappingURL=_next-internal_server_app_%5Blocale%5D_training_page_actions_1rr5ycl.js.map
